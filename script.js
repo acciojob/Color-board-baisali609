61e96f326a9f659a23d143b19594d2ec69d8c866
baisali609 <baisali609@gmail.com>
2025-02-17 22:32:44 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
 //your JS code here. If required.
+const container = document.querySelector('.container');
+
+// Create 800 boxes dynamically
+for (let i = 0; i < 800; i++) {
+  const square = document.createElement('div');
+  square.classList.add('square');
+  container.appendChild(square);
+}
