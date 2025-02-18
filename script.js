16e5afc639a99f87c3c5c9e59ceda2721cc06c57
baisali609 <baisali609@gmail.com>
2025-02-18 09:24:51 +0530

    created a submission

--- script.js
+++ script.js
@@ -4,4 +4,12 @@ for (let i = 0; i < 800; i++) {
   const square = document.createElement('div');
   square.classList.add('square');
   containers.appendChild(square);
+
+	square.addEventListener("mouseover", ()=>{
+		square.style.backgroundColor ="red";
+	})
+
+	square.addEventListener("mouseout", ()=>{
+		square.style.backgroundColor ="black";
+	})
 }
